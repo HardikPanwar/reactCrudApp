ce01c8d3ffcbcad3fc71bfa366547fecba11d88f
Hardik panwar <hardikpanwar63@gmail.com>
2022-09-25 20:15:11 +0530

    initial changes

---
+++ client/src/pages/AddEdit.js
@@ -0,0 +1,103 @@
+
+import React ,{useState, useEffect} from "react";
+import {useNavigate, useParams, Link } from "react-router-dom";
+import axios from "axios";
+import {toast} from "react-toastify";
+import "./AddEdit.css"
+const initialstate={
+    name:"",
+    email:"",
+    contact:""
+}
+const AddEdit = ()=>{
+    const [state,setState]=useState(initialstate);
+    const {name, email, contact}=state;
+    const navigate =useNavigate();
+
+    const {id}=useParams();
+
+    useEffect(()=>{
+     axios.get(`http://localhost:5000/api/get/${id}`).then((resp)=>setState({...resp.data[0]}));
+    },[id]);
+
+    const HandleSubmit=(e)=>{
+       e.preventDefault();
+       if(!name ||!email ||!contact){
+        toast.error("Please enter the value in each input field");
+       }else{
+        if(!id){
+            axios.post("http://localhost:5000/api/post",{
+            name,
+            email,
+            contact
+        }).then(()=>{
+            setState({name:"" , email:"", contact:""});
+        }).catch((err)=>{ console.log(err.response.data);})
+        toast.success("Contact Added Sucessfully")
+        }else{
+            axios.put(`http://localhost:5000/api/update/${id}`,{
+                name,
+                email,
+                contact
+            }).then(()=>{
+                setState({name:"" , email:"", contact:""});
+            }).catch((err)=>{ console.log(err.response.data);})
+            toast.success("Contact Updated Sucessfully")
+        }
+        setTimeout(()=> navigate('/'),500);
+
+       }
+    };
+    const HandleInputChange=(e)=>{
+        const {name , value}=e.target;
+        setState({... state,[name]: value})
+    }
+
+
+    return(
+        <div style={{marginTop:"100px"}}>
+            <form style={{
+                margin:"auto",
+                padding:"15px",
+                maxWidth:"400px",
+                alignContent:"center"
+
+            }}
+
+            onSubmit={HandleSubmit}
+            >
+                <label htmlFor="name">Name</label>
+                <input type="text"
+                id="name"
+                name="name"
+                placeholder="Enter your Name"
+                value={name || ""}
+                onChange={HandleInputChange} />
+
+                <label htmlFor="email">Email</label>
+                <input type="text"
+                id="email"
+                name="email"
+                placeholder="Enter your Email"
+                value={email || ""}
+                onChange={HandleInputChange} />
+
+                <label htmlFor="contact">Contact</label>
+                <input type="number"
+                id="contact"
+                name="contact"
+                placeholder="Enter your contact"
+                value={contact || ""}
+                onChange={HandleInputChange} />
+
+                <input type="submit" value="save" />
+                <Link to="/">
+                    <input type="button" value="Go Back" />
+                </Link>
+
+            </form>
+            
+        </div>
+    );
+}
+export default AddEdit;
---
+++ index.js
@@ -0,0 +1,101 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "Hardik2002",
+  database: "crud_contact",
+});
+
+dotenv.config({path : './config.env'});
+const PORT = process.env.PORT;
+
+
+if(process.env.NODE_ENV == "production"){
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'views', 'build', 'index.ejs'));
+  })
+}
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/get",(req ,res)=>{
+    const sqlGet = "SELECT * FROM contact_db";
+    db.query(sqlGet ,(err,result)=>{
+        res.send(result);
+    });
+});
+
+app.delete("/api/remove/:id",(req,res)=>{
+    const {id}=req.params;
+    const sqlRemove ="DELETE FROM contact_db WHERE id = ?";
+    db.query(sqlRemove,id,(error,result)=>{
+        if(error){
+          console.log(error);
+        }
+    });
+  
+  });
+app.get("/api/get/:id",(req ,res)=>{
+    const {id} =req.params;
+    const sqlGet = "SELECT * FROM contact_db WHERE id=?";
+    db.query(sqlGet , id,(err,result)=>{
+      if(err){
+        console.log(err);
+      }
+        res.send(result);
+    });
+});
+
+app.put("/api/update/:id",(req ,res)=>{
+  const {id} =req.params;
+  const {name ,email,contact}=req.body;
+  const sqlUpdate = "UPDATE contact_db SET name = ?, email = ?,contact = ? WHERE id = ?";
+  db.query(sqlUpdate ,[name, email, contact ,id],(err,result)=>{
+    if(err){
+      console.log(err);
+    }
+      res.send(result);
+  });
+});
+
+app.post("/api/post",(req,res)=>{
+  const {name, email, contact}=req.body;
+  const sqlInsert ="INSERT INTO contact_db (name, email, contact) VALUES (?, ?, ?)";
+  db.query(sqlInsert,[name, email, contact],(error,result)=>{
+      if(error){
+        console.log(error);
+      }
+  });
+
+});
+
+    
+
+
+
+
+
+app.get("/", (req, res) => {
+//   const sqlInsert =
+//     "INSERT INTO contact_db (name, email, contact) VALUES ('Xyz', 'xyz@gmail.com' ,021234)";
+//   db.query(sqlInsert, (err, result) => {
+//     console.log("Err", err);
+//     console.log("Result", result);
+//     res.send("Home page");
+//   });
+  
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
